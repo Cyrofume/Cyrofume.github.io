65687e3b052078001c8961b25262b3abb6affc4d
unknown <angelpg@vt.edu>
2025-06-11 20:40:10 -0400

    fix navbar color on mobile, add navbar display

--- script/script.js
+++ script/script.js
@@ -1,20 +1,27 @@
 // add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar-dark');
-console.log(header)
+const header = document.querySelector(".navbar")
+// const cols = document.querySelector(".navbar-collapse")
+
 window.onscroll = function() {
     const top = window.scrollY;
+    
     if(top >=100) {
+        // header.classList.remove('navbar-collapseHome');
         header.classList.add('navbarDark');
     }
+    // if(top >=99) {
+    //     header.classList.remove('navbar-collapseHome');
+    //     header.classList.add('navbar-collapseHome');
+    // }
     else {
         header.classList.remove('navbarDark');
+        // header.classList.remove('navbar-collapseHome');
     }
 }
+// // collapse navbar after click on small devices
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
 
-// collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
-
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+// })
